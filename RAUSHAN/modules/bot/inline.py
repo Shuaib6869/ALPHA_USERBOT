94319dae3717007c10b39e683ca8479952148a23
Shuaib6869 <Suvebansari820@gmail.com>
2024-11-14 09:18:37 +0530

    Update inline.py

--- RAUSHAN/modules/bot/inline.py
+++ RAUSHAN/modules/bot/inline.py
@@ -64,12 +64,12 @@ async def alive_function(message: Message, answers):
         InlineQueryResultArticle(
             title="Alive",
             description="Check Bot's Stats",
-            thumb_url="https://graph.org/file/3d2a5e4462b7703c949dc.jpg",
+            thumb_url="https://envs.sh/Lqy.jpg",
             input_message_content=InputTextMessageContent(
                 msg, parse_mode=ParseMode.HTML, disable_web_page_preview=True
             ),
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("──「 𝗔𝗟𝗣𝗛𝗔-𝗕𝗔𝗕𝗬 」──", callback_data="helper")]]
+                [[InlineKeyboardButton("──「 DEMON-𝗕𝗔𝗕𝗬 」──", callback_data="helper")]]
             ),
         )
     )
@@ -82,7 +82,7 @@ async def help_function(answers):
         InlineQueryResultArticle(
             title="Help Article!",
             description="Check Command List & Help",
-            thumb_url="https://graph.org/file/3d2a5e4462b7703c949dc.jpg",
+            thumb_url="https://envs.sh/Lqy.jpg",
             input_message_content=InputTextMessageContent(
                 Data.text_help_menu.format(len(CMD_HELP))
             ),
